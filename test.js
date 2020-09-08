4e16b54e237fb9f26d10741c7a3446d0fac6377e
illia.kovtun <illia.kovtun.work@gmail.com>
2020-09-08 17:46:56 +0300

    tets

--- test.js
+++ test.js
@@ -1,25 +1,31 @@
 require('chromedriver');
 const assert = require('assert');
-const {Builder, Key, By, until} = require('selenium-webdriver');
+const {
+  Builder,
+  Key,
+  By,
+  until
+} = require('selenium-webdriver');
 
-describe('Checkout Google.com', function () {
-    let driver;
+describe('Checkout progect in page', function() {
+  let driver;
 
-    before(async function() {
-        driver = await new Builder().forBrowser('chrome').build();
-    });
+  before(async function() {
+    driver = await new Builder().forBrowser('chrome').build();
+  });
 
-    it('Search on Google', async function() {
-        await driver.get('https://google.com');
-        await driver.findElement(By.name('q')).click();
-        await driver.findElement(By.name('q')).sendKeys('dalenguyen', Key.RETURN);
-        // await driver.wait(until.titleIs('dalenguyen - Google Search'), 1000);
-        await driver.wait(until.elementLocated(By.id('rcnt')), 10000);
-        await driver.findElement(By.name('q')).clear();
+  it('Search on Google', async function() {
+    await driver.get('https://dashboard-test.sken.ai');
+    await driver.sleep(3000);
+    await driver.findElement(By.id("okta-signin-username")).sendKeys('testfortestfortes@sharklasers.com', Key.RETURN);
+    await driver.findElement(By.id("okta-signin-password")).sendKeys('foSDHzuxRpVZ1', Key.RETURN);
+    await driver.findElement(By.id("okta-signin-submit")).click();
 
-        let title = await driver.getTitle();
-        assert.equal(title, 'dalenguyen - Google Search');              
-    });
+    await driver.wait(until.elementLocated(By.className("topbar__logo")), 10000);
+    // let title = await driver.getTitle();
+    // assert.equal(title, 'dalenguyen - Google Search');
 
-    after(() => driver && driver.quit());
+  });
+
+  after(() => driver && driver.quit());
 })
